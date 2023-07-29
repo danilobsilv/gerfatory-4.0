0a8386197a1426491bf08185c16f34736ca5ecdc
Danilo Bruno <danilobsilv@gmail.com>
2023-07-29 15:32:10 -0400

    função get_user

---
+++ questão 6/user_utils.py
@@ -0,0 +1,3 @@
+# considerando que essa função já exista e seguindo os passos dados na prova:
+def get_user_id_from_token(token):
+      return "usuario_aprovado" if token == "token_valido" else None
